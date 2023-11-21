af3d908f1f976693a4d88957d7afb02d94ec334f
ldemyun <129907566+ldemyun@users.noreply.github.com>
2023-11-21 13:00:24 -0500

    Group Page and New Image Assets for Groups Page

---
+++ pages/Team-page.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Meet the Team</title>
+    <meta charset="UTF-8">
+    <title>USC Hikes:Groups</title>
+    <link rel="stylesheet" href="../css/styles.css" type="text/css">
+    <link rel="stylesheet" href="../css/typography.css" type="text/css">
+    <link rel="stylesheet" href="../css/colors.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@500;600&display=swap" rel="stylesheet">
+<style>
+
+</style>
+</head>
+
+<body>
+
+</body>
+</html>
