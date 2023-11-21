55f8faff1be45a4a7be5efc13db0ae10cf3bc6ef
Byeongjun Moon <32949241+bjmoonn@users.noreply.github.com>
2023-11-20 23:30:10 -0800

    globals -> styles

--- pages/results.php
+++ pages/results.php
@@ -2,7 +2,9 @@
 <head>
     <meta charset="UTF-8">
     <title>USC Hikes</title>
-    <link rel="stylesheet" href="globals.css" type="text/css">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link rel="stylesheet" href="css/typography.css" type="text/css">
+    <link rel="stylesheet" href="css/colors.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@500;600&display=swap" rel="stylesheet">
 </head>
 <body>
