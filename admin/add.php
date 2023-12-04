72ae7415851dd40c8bc8d75b8acda18413bb0e51
Byeongjun Moon <32949241+bjmoonn@users.noreply.github.com>
2023-12-04 00:30:29 -0600

    nav & footer in admin pages, styling

--- admin/add.php
+++ admin/add.php
@@ -48,8 +48,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Add New Hike</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+        <link rel="stylesheet" href="css/typography.css" type="text/css">
+        <link rel="stylesheet" href="css/colors.css" type="text/css">
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@500;600&display=swap" rel="stylesheet">
 </head>
 <body>
+<div class="nav">
+        <div class="logo">
+            <a href="index.php"><img src="public/assets/icons/green logo.png"></a>
+        </div>
+        <div class="nav-items">
+            <!--<text class="body bold"><a href="pages/map-page.php">Map</a></text>-->
+            <text class="body bold"><a href="pages/groupPage.php">Groups</a></text>
+            <text class="body bold"><a href="pages/login.php">Log-in</a></text>
+            <text class="body bold"><a href="pages/profilepage.php">Your Profile</a></text>
+            <!-- login/profile button to be changed to dynamic when log in flow is complete -->
+        </div>
+    </div>
     <h1>Add New Hike</h1>
     <form action="add.php" method="post">
         Name: <input type="text" name="name"><br>
@@ -64,5 +80,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         Image URL: <input type="text" name="imageURL"><br>
         <input type="submit" value="Add Hike">
     </form>
+    <div class="footer">
+            <img class="footer-logo" src="public/assets/icons/logotype bottom.png">
+            <div class="footer-links">
+                <a href="../pages/TeamPage.php">Team</a>
+                <a href="../pages/faq.html">FAQ</a>
+            </div>
+     </div>
 </body>
 </html>
--- admin/index.php
+++ admin/index.php
@@ -16,10 +16,25 @@ $result = $mysql->query("SELECT * FROM mainView");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/styles.css" type="text/css">
+        <link rel="stylesheet" href="css/typography.css" type="text/css">
+        <link rel="stylesheet" href="css/colors.css" type="text/css">
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@500;600&display=swap" rel="stylesheet">
     <title>Admin Dashboard</title>
-    <!-- Link to a different CSS ? -->
 </head>
 <body>
+    <div class="nav">
+        <div class="logo">
+            <a href="index.php"><img src="public/assets/icons/green logo.png"></a>
+        </div>
+        <div class="nav-items">
+            <!--<text class="body bold"><a href="pages/map-page.php">Map</a></text>-->
+            <text class="body bold"><a href="pages/groupPage.php">Groups</a></text>
+            <text class="body bold"><a href="pages/login.php">Log-in</a></text>
+            <text class="body bold"><a href="pages/profilepage.php">Your Profile</a></text>
+            <!-- login/profile button to be changed to dynamic when log in flow is complete -->
+        </div>
+    </div>
     <div id="admin-nav">
         <a href="add.php">Add Hike</a>
         <a href="edit.php">Edit Hike</a>
@@ -49,6 +64,13 @@ $result = $mysql->query("SELECT * FROM mainView");
         }
         ?>
     </div>
+    <div class="footer">
+            <img class="footer-logo" src="public/assets/icons/logotype bottom.png">
+            <div class="footer-links">
+                <a href="../pages/TeamPage.php">Team</a>
+                <a href="../pages/faq.html">FAQ</a>
+            </div>
+     </div>
 </body>
 </html>
 
